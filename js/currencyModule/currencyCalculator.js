e845b41c63d48c7cd17929f25cd4066b8c102270
SweetBroHellaCode <emaron@Zips-MacBook-Pro-41.local>
2015-09-12 18:16:35 -0400

    Added functionality to currencyCalculator so that gold and silver convert to grams in relation to the dollar

--- js/currencyModule/currencyCalculator.js
+++ js/currencyModule/currencyCalculator.js
@@ -3,11 +3,19 @@ function currencyCalculatorFactory() {
 	/**
 	* Array to hold 
 	*/
+	var finalBitcoinAmount;
+
+	var finalGoldAmount;
+
+	var finalSilverAmount;
+
 	var currencyCalculator = new Object();
 
-	var userInputAmount = prompt("Enter number in USD");
+	var userInputAmount = prompt("Enter amount in USD");
 
-	currencyCalculator.currencyObjectCollection = [];
+	currencyCalculator.currencyObjectCollectionUSD = [];
+
+	currencyCalculator.currencyObjectCollectionGrams = [];
 
 	/**
 	* Request for the JSON data amounts 
@@ -19,12 +27,18 @@ function currencyCalculatorFactory() {
 			if((request.status == 200) && (request.readyState == 4)) {
 				var convertedJSON = JSON.parse(request.responseText);
 				for(key in convertedJSON){
-					currencyObj = currencyFactory(key, convertedJSON[key].USD);
-					currencyCalculator.currencyObjectCollection.push(currencyObj);
+					currencyObjUSD = currencyFactory(key, convertedJSON[key].USD);
+					currencyCalculator.currencyObjectCollectionUSD.push(currencyObjUSD);
+
+					currencyObjGrams = currencyFactory(key, convertedJSON[key].Grams);
+					currencyCalculator.currencyObjectCollectionGrams.push(currencyObjGrams);
 					
 				}
-				console.log(currencyCalculator.currencyObjectCollection);
-				currencyCalculator.convertAmount(currencyCalculator.currencyObjectCollection);
+				
+				currencyCalculator.convertAmountBitcoin(currencyCalculator.currencyObjectCollectionUSD);
+				currencyCalculator.convertAmountGoldSilver(currencyCalculator.currencyObjectCollectionGrams);
+				
+				currencyCalculator.printData();
 			}
 		}
 		request.send();
@@ -34,25 +48,47 @@ function currencyCalculatorFactory() {
 	/**
 	* Conversion of JSON data amounts into usable currency amounts  
 	*/
-	currencyCalculator.convertAmount = function (data) {
+	currencyCalculator.convertAmountBitcoin = function (data) {
 		for (key in data) {
 			var bitcoinAmount = (data[0].convertValue);
-			var goldAmount = (data[1].convertValue);
-			var silverAmount = (data[2].convertValue);
+			//var goldAmount = (data[1].convertValue);
+			//var silverAmount = (data[2].convertValue);
 			
 		}
-		var finalBitcoinAmount = bitcoinAmount *= userInputAmount;
-		var finalGoldAmount = goldAmount *= userInputAmount;
-		var finalSilverAmount = silverAmount *= userInputAmount;
+		 finalBitcoinAmount = bitcoinAmount *= userInputAmount;
+		//var finalGoldAmount = goldAmount *= userInputAmount;
+		//var finalSilverAmount = silverAmount *= userInputAmount;
 
 		//Displays final result in an alert box
-		alert("Bitcoin equals " + finalBitcoinAmount + "\n" + "Gold equals " + finalGoldAmount + "\n" + "Silver equals " + finalSilverAmount);
+		//alert("Bitcoin equals " + finalBitcoinAmount + "\n" + "Gold equals " + finalGoldAmount + "\n" + "Silver equals " + finalSilverAmount);
 			
 	}
 
+	currencyCalculator.convertAmountGoldSilver = function (data) {
+		for (key in data) {
+			var goldAmount = (data[0].convertValue);
+			var silverAmount = (data[1].convertValue);
+			
+		}
+
+		 finalGoldAmount = goldAmount *= userInputAmount;
+		 finalSilverAmount = silverAmount *= userInputAmount;
+	}
 
 	/**
-	*Calls to have currency conversion data on screen
+	*Alerts user with final outputs
+	*/
+	currencyCalculator.printData = function () {
+		alert("Bitcoin equals " +
+		 finalBitcoinAmount + "dollars" +
+		  "\n" + "Gold equals " +
+		   finalGoldAmount + " grams" +
+		    "\n" + "Silver equals " +
+		    finalSilverAmount + " grams");
+	}	
+
+	/**
+	*Calls to get currency conversion data
 	*/
 	function init(){
 		currencyCalculator.requestData('http://coinabul.com/api.php');	
@@ -60,5 +96,7 @@ function currencyCalculatorFactory() {
 	
 	init();
 }
+
+
 currencyCalculatorFactory();
 
