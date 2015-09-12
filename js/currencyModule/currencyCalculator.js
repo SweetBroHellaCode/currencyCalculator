c2bce5ec0b59e5dc28fa267b5005d388c096ff22
SweetBroHellaCode <emaron@Zips-MacBook-Pro-41.local>
2015-09-12 16:59:11 -0400

    Got it working, but silver and gold do not output correct response

--- js/currencyModule/currencyCalculator.js
+++ js/currencyModule/currencyCalculator.js
@@ -5,6 +5,8 @@ function currencyCalculatorFactory() {
 	*/
 	var currencyCalculator = new Object();
 
+	var userInputAmount = prompt("Enter number in USD");
+
 	currencyCalculator.currencyObjectCollection = [];
 
 	/**
@@ -12,7 +14,6 @@ function currencyCalculatorFactory() {
 	*/
 	currencyCalculator.requestData = function (url) { 
 		var request = new XMLHttpRequest();
-			console.log("Heey");
 		request.open('GET', url);
 		request.onreadystatechange = function() {
 			if((request.status == 200) && (request.readyState == 4)) {
@@ -23,7 +24,7 @@ function currencyCalculatorFactory() {
 					
 				}
 				console.log(currencyCalculator.currencyObjectCollection);
-				
+				currencyCalculator.convertAmount(currencyCalculator.currencyObjectCollection);
 			}
 		}
 		request.send();
@@ -33,11 +34,25 @@ function currencyCalculatorFactory() {
 	/**
 	* Conversion of JSON data amounts into usable currency amounts  
 	*/
+	currencyCalculator.convertAmount = function (data) {
+		for (key in data) {
+			var bitcoinAmount = (data[0].convertValue);
+			var goldAmount = (data[1].convertValue);
+			var silverAmount = (data[2].convertValue);
+			
+		}
+		var finalBitcoinAmount = bitcoinAmount *= userInputAmount;
+		var finalGoldAmount = goldAmount *= userInputAmount;
+		var finalSilverAmount = silverAmount *= userInputAmount;
 
+		//Displays final result in an alert box
+		alert("Bitcoin equals " + finalBitcoinAmount + "\n" + "Gold equals " + finalGoldAmount + "\n" + "Silver equals " + finalSilverAmount);
+			
+	}
 
 
 	/**
-	*Display currency data on screen
+	*Calls to have currency conversion data on screen
 	*/
 	function init(){
 		currencyCalculator.requestData('http://coinabul.com/api.php');	
@@ -46,3 +61,4 @@ function currencyCalculatorFactory() {
 	init();
 }
 currencyCalculatorFactory();
+
